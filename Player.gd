60b1ad61c7bdbcfc599af129510348e3ecafa916
analog_wizard <thecoolmathdude@gmail.com>
2024-04-13 02:26:37 -0500

    Head included and new organization

--- Player.gd
+++ Player.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+@onready var left_arm = get_node("Limbs/Left Arm")
+@onready var left_fore_arm = get_node("Limbs/Left Forearm")
+
+@onready var right_arm = get_node("Limbs/Right Arm")
+@onready var right_fore_arm = get_node("Limbs/Right Forearm")
+
+@onready var left_leg = get_node("Limbs/Left Leg")
+@onready var left_leg_shin = get_node("Limbs/Left Shin")
+
+@onready var right_leg = get_node("Limbs/Right Leg")
+@onready var right_leg_shin = get_node("Limbs/Right Shin")
+
+func flex_right():
+	right_arm.linear_damp = 10
+	right_fore_arm.linear_damp = 10
+	right_fore_arm.angular_velocity = -10
+	right_arm.angular_velocity = -10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
+	# Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	flex_right()
